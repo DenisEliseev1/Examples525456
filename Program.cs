fb8087d4004cd18475e9d565f06e7e2ad7d8a819
Denis Eliseev <medved-ed@mail.ru>
2022-08-20 00:14:05 +0800

    Examples 545658

--- Program.cs
+++ Program.cs
@@ -1,23 +1,52 @@
 ﻿//Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
 void Zadacha54 ()
 {
-
+    Random random = new Random();
+    int rows = random.Next(2, 5);
+    int coloms = random.Next(4, 10);
+    int[,] array = new int[rows, coloms];
+    FillArray (array);
+    PrintArray (array);
+    Console.WriteLine();
+    SortArrayRows (array);
+    PrintArray (array);
 }
 
 
 //Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
-//Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+//Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов.
 void Zadacha56 ()
 {
-
+    Random random = new Random();
+    int rows = random.Next(4, 10);
+    int coloms = random.Next(4, 10);
+    int[,] array = new int[rows, coloms];
+    FillArray (array);
+    PrintArray (array);
+    Console.WriteLine("Сумма строк элементов: ");
+    int [] sumRowsArray = new int [rows];
+    int min = 0;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < coloms; j++)
+        {
+            sumRowsArray[i] += array [i,j];
+        }
+        Console.WriteLine($"{i+1} строка - {sumRowsArray[i]}");
+        if ((i > 0) && (sumRowsArray[i] < sumRowsArray[min])) 
+        {
+            min = i;
+        } 
+    }
+    Console.WriteLine ($"Строка с минимальным суммой элементов - {min+1}");
 }
 
 //Задача 58. Заполните спирально массив 4 на 4.
 void Zadacha58()
 {   
     Random random = new Random();
-    int rows = random.Next(4, 10);
-    int coloms = random.Next(4, 10);
+    int rows = random.Next(4, 13);
+    int coloms = random.Next(4, 13);
     int[,] array = new int[rows, coloms];
     int count = 1;
     int startOne = 0;
@@ -28,38 +57,33 @@ void Zadacha58()
     int finishThree = 0;
     int startFour = rows - 2;
     int finishFour = 1;
-    int fixNumber = 0;
     int weight = coloms * rows;
     while (count < weight)
     {    
         for (int i = startOne; i <= finishOne; i++)
             {
                 if (weight < count) break;
-                array [fixNumber, i] = count;
+                array [(finishFour-1), i] = count;
                 count++;
             }
-        fixNumber = finishOne;
         for (int i = startTwo; i <= finishTwo; i++)
             {
                 if (weight < count) break;
-                array [i, fixNumber] = count;
+                array [i, finishOne] = count;
                 count++;                
             }
-        fixNumber = finishTwo;
         for (int i = startThree; i >= finishThree; i--)
             {
                 if (weight < count) break;
-                array [fixNumber, i] = count;
+                array [finishTwo, i] = count;
                 count++;  
             }
-        fixNumber = finishThree;
         for (int i = startFour; i >= finishFour; i--)
             {
                 if (weight < count) break;
-                array [i, fixNumber] = count;
+                array [i, finishThree] = count;
                 count++;
             }
-        fixNumber = finishFour;
         finishOne --;
         startOne ++;
         finishTwo --;
@@ -102,6 +126,28 @@ void PrintArray(int[,] array)
     }
 }
 
+void SortArrayRows (int [,] array)
+{
+    int temp = 0;
+    int rows = array.GetLength(0);
+    int coloms = array.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < coloms; j++)
+        {
+            for (int k = j; k < coloms; k++)
+            {
+                if (array [i, j] > array [i, k])
+                {
+                    temp = array [i, j];
+                    array [i, j] = array [i, k];
+                    array [i, k] = temp;                
+                }
+            }
+        }
+    }
+}
 
-
-Zadacha58();
+//Zadacha54();
+Zadacha56();
+//Zadacha58();
